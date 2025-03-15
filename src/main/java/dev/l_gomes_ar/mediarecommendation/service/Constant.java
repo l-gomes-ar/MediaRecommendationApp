486dc3fcf9ebc1b4f7f6f6ead286b8afab319a17
l-gomes-ar <lgaraujo021@gmail.com>
2025-03-15 23:09:16 +0000

    Added link to themoviedb for getting a valid API token

--- src/main/java/dev/l_gomes_ar/mediarecommendation/service/Constant.java
+++ src/main/java/dev/l_gomes_ar/mediarecommendation/service/Constant.java
@@ -1,5 +1,7 @@
 package dev.l_gomes_ar.mediarecommendation.service;
 
 public class Constant {
+    // Update this property with a valid token from TMdb
+    // https://developer.themoviedb.org/reference/intro/getting-started
     public static final String API_TOKEN = "HIDDEN_API_KEY_HERE";
 }
